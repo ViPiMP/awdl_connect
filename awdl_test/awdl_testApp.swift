3ab317317f9c2263645d4a40b311f24ac38e0756
junjiecui <2871843852@qq.com>
2026-03-31 10:43:24 +0800

    Initial Commit

---
+++ awdl_test/awdl_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  awdl_testApp.swift
+//  awdl_test
+//
+//  Created by junjiecui on 2026/3/31.
+//
+
+import SwiftUI
+
+@main
+struct awdl_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
